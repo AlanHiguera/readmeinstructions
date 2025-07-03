e47446825c838709567d30e0b9b733c1e3772ef1
AlanHiguera <alanhiguera144@gmail.com>
2025-07-02 20:45:46 -0400

    Create Config.dart

---
+++ Config.dart
@@ -0,0 +1,16 @@
+class Config {
+  static const String appName = "Handmade Geeks";
+  static const String apiURL = '192.168.1.91';
+  static const obtenerProductoMainAPI = "api/producto/obtenerproductomain";
+  static const productoAPI = "api/producto";
+  static const tiendaAPI = "api/tienda";
+  static const loginAPI = "api/login/";
+  static const obtenertokenAPI = "api/api-token-auth/";
+  static const productoadminAPI = "api/productoadmin/";
+  static const usuarioAPI = "api/usuario/";
+  static const administradorAPI = "api/administrador";
+  static const productodeseadoAPI = "api/productodeseado";
+  static const tipocategoriaAPI = "api/tipocategoria";
+  static const carritoAPI = "api/carrito";
+  static const seguimientotiendaAPI = "api/seguimientotienda";
+}
